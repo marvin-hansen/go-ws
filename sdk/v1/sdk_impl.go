0a5d5b1bf4ab4c5013e30f7e95d47bfeeef6eb12
Marvin <marvin.hansen@gmail.com>
2021-07-08 17:01:15 +0800

    Restructured code

--- sdk/v1/sdk_impl.go
+++ sdk/v1/sdk_impl.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"go-ws/sdk/types"
 	"go-ws/sdk/web_socket"
+	"log"
 )
 
 type SDKImpl struct {
@@ -27,4 +28,29 @@ func (s SDKImpl) init(url string) {
 	ws := web_socket.NewWebSocket(url)
 	s.url = &url
 	s.ws = ws
+}
+
+func (s SDKImpl) OpenConnection() (err error) {
+	url := *s.url
+	err = s.ws.Connect(url, nil)
+	return err
+}
+
+func (s SDKImpl) CloseConnection() (err error) {
+	running = false // Stop processing messages
+	err = s.ws.Close()
+	if err != nil {
+		log.Println("Can't close connection")
+		log.Println(err)
+	}
+	return err
+}
+
+func (s SDKImpl) ResetConnection() (err error) {
+	err = s.CloseConnection()
+	logError(err)
+
+	err = s.OpenConnection()
+	logError(err)
+	return err
 }
