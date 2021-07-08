b08879f240ed34c0af22a8bd297b88a16bcc67bb
Marvin <marvin.hansen@gmail.com>
2021-07-08 20:33:16 +0800

    Removed dead code that prevented build

--- sdk/v1/set_methods.go
+++ sdk/v1/set_methods.go
@@ -5,10 +5,3 @@ import "go-ws/sdk/types"
 func (s SDKImpl) SetErrorInvoke(function types.InvokeFunction) {
 	errorMessageInvoke = function
 }
-
-func (s SDKImpl) SetHeartBeatInvoke(function types.InvokeFunction) {
-	heartBeatInvoke = function
-}
-
-func (s SDKImpl) SetReconnectInvoke(function types.InvokeFunction) {
-}
