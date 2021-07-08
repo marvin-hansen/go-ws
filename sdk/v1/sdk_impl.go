cd2f181d1e10b3c757f873355f7ae19460d23835
Marvin <marvin.hansen@gmail.com>
2021-07-08 16:58:52 +0800

    Embedded standalone web socket into streaming SDK

--- sdk/v1/sdk_impl.go
+++ sdk/v1/sdk_impl.go
@@ -1,30 +1,30 @@
 package v1
 
 import (
-	"github.com/gorilla/websocket"
 	"go-ws/sdk/types"
+	"go-ws/sdk/web_socket"
 )
 
 type SDKImpl struct {
+	url *string
+	ws  *web_socket.WebSocket
 }
 
 var (
-	con     *websocket.Conn
-	stopC   chan struct{}
-	running bool
-	//
-	// sys handlers
+	running         bool
 	errorInvoke     types.InvokeFunction
 	heartBeatInvoke types.InvokeFunction
 	reconnectInvoke types.InvokeFunction
 )
 
-func NewOemlSdkV1() (sdk *SDKImpl) {
+func NewOemlSdkV1(url string) (sdk *SDKImpl) {
 	sdk = new(SDKImpl)
-	sdk.init()
+	sdk.init(url)
 	return sdk
 }
 
-func (s SDKImpl) init() {
-
+func (s SDKImpl) init(url string) {
+	ws := web_socket.NewWebSocket(url)
+	s.url = &url
+	s.ws = ws
 }
