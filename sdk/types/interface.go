09296182d89b091ca9d8e0d98233b0bbe9ab3855
Marvin <marvin.hansen@gmail.com>
2021-07-15 20:51:51 +0800

    Added interface methods to exposed order methods

--- sdk/types/interface.go
+++ sdk/types/interface.go
@@ -5,12 +5,18 @@ type SDK interface {
 	CloseConnection() (err error)
 	ResetConnection() (err error)
 	//
-	LookupSymbolData(exchangeID, baseSymbolCoinApi, quoteSymbolCoinApi string) (ok bool, symbolData SymbolData)
+	LookupSymbolData(exchangeID, baseSymbolCoinApi, quoteSymbolCoinApi string) (symbolData SymbolData, ok bool)
+	NewSingleOrderRequest(symbolData SymbolData, exchangeId string, clientOrderId string, amountOrder float64, price float64, side OrdSide, orderType OrdType, timeInForce TimeInForce) (req *OrderNewSingleRequest)
+	NewCancelSingleOrderRequest(exchangeId, exchangeOrderId, clientOrderId string) (req *OrderCancelSingleRequest)
+	NewCancelAllOrdersRequest(exchangeId string) (req *OrderCancelAllRequest)
+	//
+	PlaceSingleOrder(req *OrderNewSingleRequest) (err error)
+	CancelSingleOrder(req *OrderCancelSingleRequest) (err error)
+	CancelAllOrders(req *OrderCancelAllRequest) (err error)
 	//
 	SetSystemInvoke(function SystemInvoke)
 	SetSnapshotInvoke(function SnapshotInvoke)
 	SetUpdateInvoke(function UpdateInvoke)
-	//
 	SetExecUpdateInvoke(function InvokeFunction)
 	SetExecSnapshotInvoke(function InvokeFunction)
 	SetBalanceUpdateInvoke(function InvokeFunction)
