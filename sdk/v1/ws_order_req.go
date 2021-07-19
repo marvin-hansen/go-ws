d114127dd55f49d7794799a4f77ba9f000eb3f70
Marvin <marvin.hansen@gmail.com>
2021-07-19 16:22:44 +0800

    Updated order req methods

--- sdk/v1/ws_order_req.go
+++ sdk/v1/ws_order_req.go
@@ -2,12 +2,11 @@ package v1
 
 import "go-ws/sdk/types"
 
-func (s SDKImpl) NewSingleOrderRequest(exchangeId, symbolIdExchange, symbolIdCoinapi string, clientOrderId string, amountOrder float64, price float64, side types.OrdSide, orderType types.OrdType, timeInForce types.TimeInForce) (req types.OrderNewSingleRequest) {
-
-	req = types.OrderNewSingleRequest{
-		MessageType:     types.ORDER_NEW_SINGLE_REQUEST,
+func (s SDKImpl) NewSingleOrderRequest(exchangeId, symbolIdCoinApi string, clientOrderId string, amountOrder float64, price float64, side types.OrdSide, orderType types.OrdType, timeInForce types.TimeInForce) (req *types.OrderNewSingleRequest) {
+	req = &types.OrderNewSingleRequest{
+		Type:            types.ORDER_NEW_SINGLE_REQUEST,
 		ExchangeId:      exchangeId,
-		SymbolIdCoinapi: &symbolIdCoinapi,
+		SymbolIdCoinapi: &symbolIdCoinApi,
 		ClientOrderId:   clientOrderId,
 		AmountOrder:     amountOrder,
 		Price:           price,
@@ -21,29 +20,25 @@ func (s SDKImpl) NewSingleOrderRequest(exchangeId, symbolIdExchange, symbolIdCoi
 // NewCancelSingleOrderRequest constructs a new cancel request.
 // One of the properties (`exchange_order_id`, `client_order_id`) is required to identify the order.
 // exchangeId: 	Identifier of the exchange from which active orders should be canceled.
-func (s SDKImpl) NewCancelSingleOrderRequest(exchangeId, exchangeOrderId, clientOrderId string) (req types.OrderCancelSingleRequest) {
+func (s SDKImpl) NewCancelSingleOrderRequest(exchangeId, clientOrderId string) (req *types.OrderCancelSingleRequest) {
 
 	r := new(types.OrderCancelSingleRequest)
-	r.MessageType = types.ORDER_CANCEL_SINGLE_REQUEST
+	r.Type = types.ORDER_CANCEL_SINGLE_REQUEST
 	r.ExchangeId = exchangeId
-
-	if exchangeOrderId == "" {
+	if clientOrderId == "" {
+		panic("Missing Client order ID")
+	} else {
 		r.ClientOrderId = &clientOrderId
 	}
-
-	if clientOrderId == "" {
-		r.ExchangeOrderId = &exchangeOrderId
-	}
-
-	return *r
+	return r
 }
 
 // NewCancelAllOrdersRequest cancels all open orders at the exchange
 // exchangeId: 	Identifier of the exchange from which active orders should be canceled.
-func (s SDKImpl) NewCancelAllOrdersRequest(exchangeId string) (req types.OrderCancelAllRequest) {
-	req = types.OrderCancelAllRequest{
-		MessageType: types.ORDER_CANCEL_ALL_REQUEST,
-		ExchangeId:  exchangeId,
+func (s SDKImpl) NewCancelAllOrdersRequest(exchangeId string) (req *types.OrderCancelAllRequest) {
+	req = &types.OrderCancelAllRequest{
+		Type:       types.ORDER_CANCEL_ALL_REQUEST,
+		ExchangeId: exchangeId,
 	}
 	return req
 }
