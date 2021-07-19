07cd8685023c5201e6d531c99f505fec0299a20d
Marvin <marvin.hansen@gmail.com>
2021-07-19 16:22:02 +0800

    Removed debug code from order handler

--- sdk/v1/ws_order_handler.go
+++ sdk/v1/ws_order_handler.go
@@ -8,32 +8,15 @@ import (
 const verbose = false
 
 func (s SDKImpl) PlaceSingleOrder(req *t.OrderNewSingleRequest) (err error) {
-
 	b, err := req.MarshalJSON()
 	logError(err)
-
-	if verbose {
-		println("PlaceSingleOrder")
-		println(string(b))
-	}
-
-	err = ws.WriteJSONMessage(b)
-	if err != nil {
-		log.Println("can't send message!")
-		logError(err)
-		return err
-	}
+	logError(err)
+	err = s.sendMessage(b)
 	return checkError(err)
 }
 
 func (s SDKImpl) CancelSingleOrder(req *t.OrderCancelSingleRequest) (err error) {
 	b, err := req.MarshalJSON()
-
-	if verbose {
-		println("CancelSingleOrder")
-		println(string(b))
-	}
-
 	logError(err)
 	err = s.sendMessage(b)
 	return checkError(err)
@@ -41,18 +24,16 @@ func (s SDKImpl) CancelSingleOrder(req *t.OrderCancelSingleRequest) (err error) 
 
 func (s SDKImpl) CancelAllOrders(req *t.OrderCancelAllRequest) (err error) {
 	b, err := req.MarshalJSON()
-
-	if verbose {
-		println("CancelAllOrders")
-		println(string(b))
-	}
-
 	logError(err)
 	err = s.sendMessage(b)
 	return checkError(err)
 }
 
 func (s SDKImpl) sendMessage(b []byte) (err error) {
+	if verbose {
+		println("Write message into Web Socket: ")
+		println(string(b))
+	}
 	err = ws.WriteByteMessage(b)
 	if err != nil {
 		log.Println("can't send message!")
